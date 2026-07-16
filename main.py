1682d53038cf19593d36194be23092d50a24989d
pankajsingodiya-del <pankajsingodiya@gmail.com>
2026-07-16 23:04:50 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,30 @@
-from sources.opensignal import get_latest_articles
+from sources.ookla import get_latest_articles
+from telegram_service import send_message
+from article_store import load_articles, save_articles
 
 articles = get_latest_articles()
+sent_articles = load_articles()
 
 print(f"Found {len(articles)} articles")
 
-for article in articles[:10]:
-    print(article["title"])
-    print(article["link"])
-    print("-" * 60)
+for article in articles:
+
+    if article["link"] in sent_articles:
+        print("Already Sent:", article["title"])
+        continue
+
+    message = f"""📡 Ookla Latest News
+
+📰 {article['title']}
+
+🔗 {article['link']}
+
+📅 {article['published']}
+"""
+
+    send_message(message)
+
+    sent_articles.append(article["link"])
+    save_articles(sent_articles)
+
+    print("New Article Sent")
