59b8e1cc731eec4972a3e8db174e1357258787cb
pankajsingodiya-del <pankajsingodiya@gmail.com>
2026-07-16 18:51:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print("HELLO FROM NEW MAIN.PY")
+from sources.ookla import get_latest_articles
+from telegram_service import send_message
+
+articles = get_latest_articles()
+
+print(f"Found {len(articles)} articles")
+
+if len(articles) == 0:
+    send_message("❌ No Ookla news found.")
+else:
+    article = articles[0]
+
+    message = f"""📡 Ookla Latest News
+
+📰 {article['title']}
+
+🔗 {article['link']}
+
+📅 {article['published']}
+"""
+
+    print(message)
+    send_message(message)
