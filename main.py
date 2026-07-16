8fdf0704b1497483a1ba06c03c9d3da8faa981f9
pankajsingodiya-del <pankajsingodiya@gmail.com>
2026-07-16 15:55:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("Telecom Intelligence Bot Started...")
+import os
+import requests
+
+BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
+CHAT_ID = os.getenv("TELEGRAM_CHAT_ID")
+
+message = """
+🚀 Telecom Intelligence Bot Started Successfully!
+
+✅ GitHub Actions Connected
+✅ Telegram Connected
+
+Next Step:
+📡 Ookla Monitoring
+📡 OpenSignal Monitoring
+
+Powered by Pankaj 🚀
+"""
+
+url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+payload = {
+    "chat_id": CHAT_ID,
+    "text": message
+}
+
+response = requests.post(url, data=payload)
+
+print(response.text)
