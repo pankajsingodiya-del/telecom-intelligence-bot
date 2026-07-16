4ad1a5f68c13dc0a54c6ecccbf48902b81fbb292
pankajsingodiya-del <pankajsingodiya@gmail.com>
2026-07-16 19:57:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,27 +1,10 @@
-from sources.ookla import get_latest_articles
-from telegram_service import send_message
-from article_store import is_sent, mark_sent
+from sources.opensignal import get_latest_articles
 
 articles = get_latest_articles()
 
 print(f"Found {len(articles)} articles")
 
-for article in articles:
-
-    if is_sent(article["link"]):
-        print("Already Sent:", article["title"])
-        continue
-
-    message = f"""📡 Ookla Latest News
-
-📰 {article['title']}
-
-🔗 {article['link']}
-
-📅 {article['published']}
-"""
-
-    send_message(message)
-    mark_sent(article["link"])
-
-    print("New Article Sent")
+for article in articles[:5]:
+    print(article["title"])
+    print(article["link"])
+    print("-" * 60)
