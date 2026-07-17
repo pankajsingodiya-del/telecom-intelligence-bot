8de155f83796ea9ecf647782e67b3c7df8208f02
pankajsingodiya-del <pankajsingodiya@gmail.com>
2026-07-17 18:55:16 +0530

    Create telecom_news.py

---
+++ sources/telecom_news.py
@@ -0,0 +1,22 @@
+import feedparser
+
+RSS_URL = "https://telecomtalk.info/feed/"
+
+
+def get_latest_articles():
+
+    feed = feedparser.parse(RSS_URL)
+
+    articles = []
+
+    for entry in feed.entries:
+
+        articles.append({
+            "title": entry.title,
+            "link": entry.link,
+            "published": entry.get("published", "")
+        })
+
+    print(f"Found {len(articles)} Telecom News articles")
+
+    return articles
